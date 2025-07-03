d12c862f500c595308ae43d176a708c530bb03c2
Alexandr Kotov <sa.cotov@yandex.ru>
2025-07-04 01:57:30 +0300

    fix: extra code has been removed from the main

--- cmd/main.go
+++ cmd/main.go
@@ -1,46 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"github.com/crafty-ezhik/carbonstats/internal/carbon"
-	logger2 "github.com/crafty-ezhik/carbonstats/logger"
-	"net/url"
 )
 
-type Response struct {
-	Call   string           `json:"call"`
-	Result []map[string]any `json:"result"`
-}
-
 func main() {
-	logger := logger2.NewLogger(true)
-	billing := carbon.NewCarbonBilling("host", 0, logger)
+	//logger := logger2.NewLogger(true)
+	billing := carbon.NewCarbonBilling("78.155.208.228", 8082)
 
-	params := map[string]any{
-		"parent__range": []int{0, 0},
-	}
-
-	paramsJson, _ := json.Marshal(params)
-
-	formData := url.Values{}
-	formData.Add("method1", "objects.filter")
-	formData.Add("arg1", string(paramsJson))
-
-	resp, _ := billing.CallApi("Abonents", []byte(formData.Encode()))
-	fmt.Println(string(resp))
-
-	var data Response
-	err := json.Unmarshal(resp, &data)
+	data, err := billing.GetAbonentsList([]int{1455, 1456})
 	if err != nil {
 		fmt.Println(err)
 	}
-	for _, item := range data.Result[0] {
+
+	for _, item := range data.Abonents {
 		fmt.Println(item)
-		if items, ok := item.(map[string]any); ok {
-			for key, value := range items {
-				fmt.Printf("Поле: %s, Значение: %v\n", key, value)
-			}
-		}
 	}
+
 }
