a97c59396eeea3cd95fac5f726034f9644054cd2
Alexandr <alexandr@biznes-len.com>
2025-07-04 17:39:13 +0300

    feat: add new type

--- internal/carbon/types.go
+++ internal/carbon/types.go
@@ -1,5 +1,8 @@
 package carbon
 
+import "github.com/shopspring/decimal"
+
+// Параметры для формирования запроса
 const (
 	MethodOne   = "method1"
 	ArgOne      = "arg1"
@@ -10,31 +13,54 @@ const (
 	Fields      = "fields"
 )
 
+// Дополнительные методы получения данных
+const (
+	MethodGetDetails = "get_details"
+)
+
+// Методы получения данных
 const (
 	ObjFilter = "objects.filter"
 	ObjAll    = "objects.all"
 	ObjGet    = "objects.get"
 )
 
+// Модели в Carbon Billing к котором делаются запросы
 const (
-	ModelAbonents = "Abonents"
+	ModelAbonents         = "Abonents"
+	ModelFinanceOperation = "FinanceOperations"
+	ModelVoipCounters     = "VoipCounters"
 )
 
+// Поля, которые можно выбрать при запросе
 const (
 	FieldName           = "\"name\""
 	FiledEmail          = "\"email\""
 	FieldContractNumber = "\"contract_number\""
 	FieldOperatorID     = "\"operator_id\""
 	FieldParentID       = "\"parent_id\""
+
+	FieldOpSumma = "\"op_summa\""
+	FieldNumber  = "\"number\""
+
+	FieldPrice  = "\"price\""
+	FieldVolume = "\"vv\""
+
+	FieldMonth           = "\"month_number\""
+	FieldYear            = "\"year_number\""
+	FieldOutgoingTraffic = "\"v_out\""
+	FieldAmount          = "\"summa\""
 )
 
 type AnyMap map[string]interface{}
 
+// Pair - структура для описания аргументов запроса
 type Pair struct {
 	Key   string
 	Value any
 }
 
+// RequestParams - структура для формирования тела запроса
 type RequestParams struct {
 	Method1 string
 	Arg1    []Pair
@@ -46,15 +72,27 @@ type RequestParams struct {
 }
 
 type Response struct {
-	Call   string         `json:"call"`
-	Result map[string]any `json:"result"`
-	Error  string         `json:"error"`
+	Call   string        `json:"call"`
+	Result ResultRequest `json:"result"`
+	Error  string        `json:"error"`
 }
 
 type ResponseWithManyRes struct {
-	Call   string           `json:"call"`
-	Result []map[string]any `json:"result"`
-	Error  string           `json:"error"`
+	Call   string          `json:"call"`
+	Result []ResultRequest `json:"result"`
+	Error  string          `json:"error"`
+}
+
+type ResponseWithManyRes2 struct {
+	Call   string          `json:"call"`
+	Result []ResultRequest `json:"result"`
+	Error  string          `json:"error"`
+}
+
+type ResultRequest struct {
+	PK     int            `json:"pk"`
+	Model  string         `json:"model"`
+	Fields map[string]any `json:"fields"`
 }
 
 type AbonentsInfoList struct {
@@ -68,4 +106,9 @@ type AbonentInfo struct {
 	Email          string `json:"email"`
 	OperatorID     int    `json:"operator_id"`
 	ParentID       int    `json:"parent_id"`
+}
+
+type DocumentInfo struct {
+	Number string          `json:"number"`
+	Amount decimal.Decimal `json:"op_summa"`
 }
